95877545daf4c5b7ba7631fd4c4eaddf5570575e
MattBasthon <matthew.bashton@newcastle.ac.uk>
2016-07-21 14:32:39 +0100

    formatting of 1,000

--- UI.R
+++ UI.R
@@ -60,7 +60,7 @@ shinyUI(fluidPage(theme = shinytheme("united"),
                            "We can efficiently impute missing values of up to 6 missing probes, if a sample has more missing values it is said to have failed Probe QC and is not classified."), p(),
                    tags$li("A multi-class optimised ", a("Support Vector Machine", href = "https://en.wikipedia.org/wiki/Support_vector_machine"), "(SVM) validated and trained on our extensive 450k medulloblastoma cohort is used to robustly assign a subgroup to samples by their 17 β values."), p(),
                    tags$ul(
-                     tags$li("Our SVM is validated using a bootstrapping technique via 1000 random iterations of 80% of the training set, confidence interval derived from this is plotted on the Classification Graph as a box plot."), p(),
+                     tags$li("Our SVM is validated using a bootstrapping technique via 1,000 random iterations of 80% of the training set, confidence interval derived from this is plotted on the Classification Graph as a box plot."), p(),
                      tags$li("The final probability assignment for a subgroup call is made by creating an SVM model with the whole 450k training set; these probabilities are given in the Classification Table in the initial tab."), p(),
                      tags$li("Calls made with a probability below our predefined threshold are considered unreliable and samples will be labeled as Unclassifiable in the Classification Table, these samples will not be plotted in the Classification Graph."), p()
                    ),
